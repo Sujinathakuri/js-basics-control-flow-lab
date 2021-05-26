d9a81914273e2b1c92f459d8acf9e7cb4ce14630
Sujinathakuri <sujchand2@gmail.com>
2021-05-25 21:07:04 -0400

    commit tree b247b9cbaa531866f921c8066bcc98558df4b5d8

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet() {
+    if (`${ride}` < 400) {
+        return "This one is on me!"
+    }
+    if (`${ride}` > 200) {
+        return 'I will gladly take your thirty bucks.'
+    }
+    if (`${ride}` > 2500) {
+        return 'No can do.'
+    } else {
+
+        // Write your code here!
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity() {
+    if (NYC === NYC) {
+        return 'Ok, sounds good.'
+
+    }
+    if (NYC !== NYC) {
+        return 'No go.'
+    } else {
+
+    } {
+
+    }
+    // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip() {
+    // Write your code here!
+    if(tip === "generous"){
+      return 'Thank you so much.'
+    } else if (tip === "not as generous"){
+      return 'Thank you.'
+    }else{
+      return 'Bye.'
+    }
+  
+
 }
